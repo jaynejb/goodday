754370630ef2d06a68ab6499503c7c5c3206fbb7
jaynejb <jayne.j.belz@gmail.com>
2013-05-27 02:53:39 +0900

    authors

---
+++ MusingsBlog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusingsBlog::Application.config.secret_token = '2e22538d2b11ea4bf9a9fbd5ee77a88ce8fc6e3f58772108918b33ca9edacc2ce5631fe053732847bc15d94ecfe0f8333f85c3daedc3be73db97531e0e9b0669'
